1b8285ac97d679b78c5a7b5c5cc895713474dcc6
lionel-wu@github.ibm.com <lionel.wu@ibm.com>
2021-03-14 21:54:56 -0400

    update welcome page

--- client/views/Welcome.vue
+++ client/views/Welcome.vue
@@ -1,6 +1,35 @@
+<style lang="less">
+@import "../style/theme.less";
+
+.Welcome-wrapper {
+	width: 100vw;
+	height: 100vh;
+}
+
+.Username {
+	color: @info-040;
+}
+
+.Button-wrapper {
+	margin-top: 60px;
+}
+</style>
+
 <template>
-	<div>
-		<l-text :value="`Hello, ${username}`" />
+	<div class="Welcome-wrapper VLayout align-center flow-center">
+		<div class="VLayout align-center">
+			<l-text value="Welcome to Sharkquila UI!" size="xxxlarge" />
+			<l-text class="Username" :value="username" size="xlarge" color="" />
+		</div>
+
+		<div class="VLayout Button-wrapper">
+			<l-button
+				value="Explore more"
+				btnStyle="Success"
+				size="xlarge"
+				@click="goGithub"
+			/>
+		</div>
 	</div>
 </template>
 
@@ -12,6 +41,12 @@ export default {
 	computed: {
 		username: function () {
 			return this.$store.state.main.username || "Unknown";
+		},
+	},
+	methods: {
+		goGithub: function (event) {
+			const url = "https://github.com/enw860/sharkquila_ui_demo";
+			window.location.href = url;
 		},
 	},
 	mounted: function () {
